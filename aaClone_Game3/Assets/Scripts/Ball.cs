b4082b38cff796b7ff818dbb51a1b6244d4163eb
Oğuzhan Töre ÖZYÜREK <37810336+otorezyurek@users.noreply.github.com>
2020-06-26 19:07:39 +0300

    First Commit
    First upload of AA game clone to github

---
+++ aaClone_Game3/Assets/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+	bool isStuck;
+
+	public float speed;
+
+	Rigidbody2D rb;
+
+	GameManager gm;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate()
+	{
+		if (isStuck == false)
+		{
+			rb.MovePosition(rb.position + Vector2.up * speed * Time.deltaTime);
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Spinner"))
+		{
+			transform.SetParent(collision.transform);
+			//collision.GetComponent<Spinner>().speed += 5;
+			//collision.GetComponent<Spinner>().speed *= -1;
+			isStuck = true;
+
+			FindObjectOfType<GameManager>().ScoreCounter();
+		}
+		else if (collision.CompareTag("Ball"))
+		{
+			FindObjectOfType<GameManager>().GameEnd();
+		}
+	}
+}
---
+++ aaClone_Game3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	bool isGameOver;
+
+	public Spinner spinner;
+	public Spawner spawner;
+
+	public Animator animator;
+
+	public Text scoreText;
+	public Button playAgainButton;
+	int score;
+	public void GameEnd()
+	{
+		if (isGameOver == true)
+		{
+			return;
+		}
+
+		spinner.enabled = false;
+		spawner.enabled = false;
+
+		animator.SetTrigger("GameOver");
+
+		isGameOver = true;
+
+		playAgainButton.gameObject.SetActive(true);
+
+        print("GAME END");
+	}
+
+	public void Restart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void ScoreCounter()
+	{
+		if(isGameOver == false) 
+		{
+			score += 1;
+			scoreText.text = score.ToString();
+		}
+		
+	}
+}
